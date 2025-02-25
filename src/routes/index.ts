f3d7c2dee72cb17907eb40b582e1a0c502d0a431
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-02-25 19:42:07 -0300

    feat: Rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
 
+import { urlRoutes } from "./urls-routes";
+import { redirectRoutes } from "./redirect-routes";
+
 const routes = Router();
+
+routes.use("/urls", urlRoutes)
+routes.use("/", redirectRoutes)
 
 
 export { routes }
